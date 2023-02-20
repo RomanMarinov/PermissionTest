a84c88026dc8f6b3bf5ec363f1d04ffdd4ebb3f0
romanmarinov <marinov37@mail.ru>
2023-02-20 23:22:02 +0300

    next commit

--- app/src/main/java/com/dev_marinov/permissiontest/MainActivity.kt
+++ app/src/main/java/com/dev_marinov/permissiontest/MainActivity.kt
@@ -3,6 +3,7 @@ package com.dev_marinov.permissiontest
 
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
@@ -19,16 +20,16 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        Log.d("4444", " MainActivity loaded")
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         val permissions: List<String> = arrayListOf(
-//            android.Manifest.permission.ACCESS_COARSE_LOCATION,
-//            android.Manifest.permission.ACCESS_FINE_LOCATION,
+           // android.Manifest.permission.ACCESS_COARSE_LOCATION,
+           // android.Manifest.permission.ACCESS_FINE_LOCATION,
 
             android.Manifest.permission.CAMERA,
             android.Manifest.permission.READ_EXTERNAL_STORAGE,
-            android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
+//            android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
         )
 
 
@@ -38,36 +39,36 @@ class MainActivity : AppCompatActivity() {
             ActivityCompat.requestPermissions(this, permissions.toTypedArray(), PackageManager.PERMISSION_GRANTED)
 
 
-            if (checkPermission(android.Manifest.permission.CAMERA)) {
-                binding.tv.visibility = View.VISIBLE
-            } else {
-                if (shouldShowRequestPermissionRationale(android.Manifest.permission.CAMERA)) {
-                    AlertDialog.Builder(this)
-                        .setMessage("Требуется разрешение камеры для захвата изображения. Предоставьте разрешение на доступ к вашей камере.")
-                        .setPositiveButton("OK") { dialogInterface, i ->
-                            dialogInterface.dismiss()
-                            ActivityCompat.requestPermissions(
-                                this,
-                                arrayOf(android.Manifest.permission.CAMERA),
-                                201
-                            )
-                        }
-                        .setNegativeButton("Cancel") { dialogInterface, i ->
-                            dialogInterface.dismiss()
-                        }
-                        .create()
-                        .show();
-                } else {
-                    ActivityCompat.requestPermissions(
-                        this,
-                        arrayOf(
-                            android.Manifest.permission.CAMERA,
-                            android.Manifest.permission.READ_EXTERNAL_STORAGE
-                        ),
-                        201
-                    )
-                }
-            }
+//            if (checkPermission(android.Manifest.permission.CAMERA)) {
+//                binding.tv.visibility = View.VISIBLE
+//            } else {
+//                if (shouldShowRequestPermissionRationale(android.Manifest.permission.CAMERA)) {
+//                    AlertDialog.Builder(this)
+//                        .setMessage("Требуется разрешение камеры для захвата изображения. Предоставьте разрешение на доступ к вашей камере.")
+//                        .setPositiveButton("OK") { dialogInterface, i ->
+//                            dialogInterface.dismiss()
+//                            ActivityCompat.requestPermissions(
+//                                this,
+//                                arrayOf(android.Manifest.permission.CAMERA),
+//                                201
+//                            )
+//                        }
+//                        .setNegativeButton("Cancel") { dialogInterface, i ->
+//                            dialogInterface.dismiss()
+//                        }
+//                        .create()
+//                        .show();
+//                } else {
+//                    ActivityCompat.requestPermissions(
+//                        this,
+//                        arrayOf(
+//                            android.Manifest.permission.CAMERA,
+//                            android.Manifest.permission.READ_EXTERNAL_STORAGE
+//                        ),
+//                        201
+//                    )
+//                }
+//            }
         }
     }
 
@@ -85,13 +86,26 @@ class MainActivity : AppCompatActivity() {
         grantResults: IntArray
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == 201) {
-            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                Toast.makeText(this, "Permission granted", Toast.LENGTH_SHORT).show()
+
+      //  Log.d("4444", " permissions=" + permissions.size + " grantResults=" + grantResults.size )
+       // if (requestCode == 201) {
+        Log.d("4444", " camera=" + grantResults[0].toString())
+        Log.d("4444", " read=" + grantResults[1].toString())
+        if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, " camera Permission granted", Toast.LENGTH_SHORT).show()
                 //openCamera()
+                binding.tvcamera.visibility = View.VISIBLE
+                  Log.d("4444", " camera=" + (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED))
             } else {
-                Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "camera Permission denied", Toast.LENGTH_SHORT).show()
             }
-        }
+            if (grantResults.isNotEmpty() && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, " read Permission granted", Toast.LENGTH_SHORT).show()
+                //openCamera()
+                binding.tvread.visibility = View.VISIBLE
+            } else {
+                Toast.makeText(this, " read Permission denied", Toast.LENGTH_SHORT).show()
+            }
+      //  }
     }
 }
